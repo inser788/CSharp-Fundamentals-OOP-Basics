f3d29a2a20e4a50ecddf9a61931c81867cc79fb2
inser788 <inser7@abv.bg>
2018-02-20 18:15:07 +0200

    add new folders with resources

---
+++ Exercises/04 DEFINING CLASSES-Exercises/DefineClass-Exercises/00_test/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Car
+{
+    private string model;
+    private double fuelAmount;
+    private double consumationFuel;
+    private double distanceTraveled;
+
+    public Car(string model, double fuelAmount, double consumationFuel)
+    {
+        this.model = model;
+        this.fuelAmount = fuelAmount;
+        this.consumationFuel = consumationFuel;
+        this.distanceTraveled = 0;
+    }
+
+    public string Model
+    {
+        get => model;
+        set => model = value;
+    }
+
+    public double FuelAmount
+    {
+        get => fuelAmount;
+        set => fuelAmount = value;
+    }
+
+    public double ConsumationFuel
+    {
+        get => consumationFuel;
+        set => consumationFuel = value;
+    }
+
+    public void Drive(int distance)
+    {
+        if (this.consumationFuel*distance<=this.fuelAmount)
+        {
+            this.distanceTraveled += distance;
+            this.fuelAmount -= this.consumationFuel * distance;
+        }
+        else
+        {
+            Console.WriteLine($"Insufficient fuel for the drive");
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.model} {this.fuelAmount:f2} {this.distanceTraveled}";
+    }
+}
+
---
+++ Exercises/04 DEFINING CLASSES-Exercises/DefineClass-Exercises/00_test/StartUp.cs
@@ -0,0 +1,61 @@
+﻿   using System;
+   using System.Collections.Generic;
+   using System.IO;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        var carsInfo = GetCarsInfo();
+        carsInfo = DriveCars(carsInfo);
+        PrintCars(carsInfo);
+    }
+
+    private static void PrintCars(Dictionary<string, Car> carsInfo)
+    {
+        foreach (var car in carsInfo)
+        {
+            Console.WriteLine(car.Value.ToString());
+        }
+    }
+
+    private static Dictionary<string, Car> DriveCars(Dictionary<string, Car> carsInfo)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input=="End")
+            break;
+            string[] tokens = input.Split();
+            string modelCar = tokens[1];
+            int distance = int.Parse(tokens[2]);
+            if (carsInfo.ContainsKey(modelCar))
+            {
+                carsInfo[modelCar].Drive(distance);
+            }
+        }
+
+        return carsInfo;
+    }
+
+    private static Dictionary<string, Car> GetCarsInfo()
+    {
+        var carsInfo=new Dictionary<string,Car>();
+        int numberOfCars = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numberOfCars; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+            string model = input[0];
+            double fuelAmount = double.Parse(input[1]);
+            double fuelConsumation = double.Parse(input[2]);
+
+            Car car=new Car(model,fuelAmount,fuelConsumation);
+            if (!carsInfo.ContainsKey(model))
+            {
+                carsInfo[model] = car;
+            }
+        }
+
+        return carsInfo;
+    }
+}
