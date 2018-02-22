24ddedb1158f290f3a49b40e9622f7bc5e6ae905
inser788 <inser7@abv.bg>
2018-02-22 22:02:13 +0200

    add 3 new Exercises 08 ENCAPSULATION - EXERCISES (3/6)

---
+++ Exercises/08 ENCAPSULATION - EXERCISES/Encapsulation-Exercises/01_Class_Box/Box.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+public class Box
+{
+    private double length;
+    private double width;
+    private double height;
+    private double surfaceArea;
+    private double lateralSurfaceArea;
+    private double volume;
+
+    public Box(double length, double width, double height)
+    {
+        Length = length;
+        Width = width;
+        Height = height;
+    }
+
+    public double Length
+    {
+        get => length;
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Length cannot be zero or negative.");
+            }
+
+            this.length = value;
+        }
+    }
+
+    public double Width
+    {
+        get => width;
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Width cannot be zero or negative.");
+            }
+
+            this.width = value;
+        }
+    }
+
+    public double Height
+    {
+        get => height;
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Height cannot be zero or negative.");
+            }
+
+            this.height = value;
+        }
+    }
+
+    //Volume = lwh
+    //Lateral Surface Area = 2lh + 2wh
+    //Surface Area = 2lw + 2lh + 2wh
+
+    public void GetSurfaceArea()
+    {
+        this.surfaceArea = 2 * (length * width + length * height + width * height);
+    }
+
+    public void GetLateralSurfaceArea()
+    {
+        this.lateralSurfaceArea = 2 * (length * height + width * height);
+    }
+
+    public void GetVolume()
+    {
+        this.volume = length * width * height;
+    }
+
+    public override string ToString()
+    {
+        return $"Surface Area - {this.surfaceArea:f2}\n" +
+               $"Lateral Surface Area - {this.lateralSurfaceArea:f2}\n" +
+               $"Volume - {this.volume:f2}";
+    }
+}
+
---
+++ Exercises/08 ENCAPSULATION - EXERCISES/Encapsulation-Exercises/01_Class_Box/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class StartUp
+{
+    static void Main()
+    {
+        try
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(length, width, height);
+            box.GetSurfaceArea();
+            box.GetLateralSurfaceArea();
+            box.GetVolume();
+            Console.WriteLine(box.ToString());
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }
+}
+
