4d89d67a36c65236ab388a6ecde6aa5d2e43a2a4
inser788 <inser7@abv.bg>
2018-02-26 21:47:42 +0200

    add all 5 Exercises from /09 INHERITANCE - LAB/Inheritance-Lab

---
+++ Exercises/09 INHERITANCE - LAB/Inheritance-Lab/04_Random_List/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class StartUp
+{
+    static void Main()
+    {
+        //var randomList = new RandomList();
+        //randomList.Add("one");
+        //randomList.Add("two");
+        //randomList.Add("three");
+
+        //Console.WriteLine(randomList.RandomString());
+    }
+}
+
---
+++ Exercises/09 INHERITANCE - LAB/Inheritance-Lab/05_Stack_of_Strings/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class StartUp
+{
+    static void Main()
+    {
+        var data = new StackOfStrings();
+        data.Push("one");
+
+        Console.WriteLine(data.IsEmpty());
+        try
+        {
+            Console.WriteLine(data.Pop());
+            Console.WriteLine(data.Peek());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
+
