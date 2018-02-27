abc289bd7a1da1582bb61cffa780c51da9a5abfe
inser788 <inser7@abv.bg>
2018-02-27 22:58:47 +0200

    add 1 new Exercises/10 INHERITANCE - EXERCISES/ 4/6

---
+++ Exercises/10 INHERITANCE - EXERCISES/Inheritance-Exercises/04_Online_Radio_Database/Song.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+public class Song
+{
+    private const int minArtistNameLength = 3;
+    private const int maxArtistNameLength = 20;
+    private const int minSongNameLength = 3;
+    private const int maxSongNameLength = 30;
+    private const int minMinutes = 0;
+    private const int maxMinutes = 14;
+    private const int minSeconds = 0;
+    private const int maxSeconds = 59;
+
+
+    private string artist;
+    private string name;
+    private int minutesDuration;
+    private int secondsDuration;
+    private long totalDurationSeconds;
+
+    public Song(string artist, string name, int minutesDuration, int secondsDuration)
+    {
+        Artist = artist;
+        Name = name;
+        MinutesDuration = minutesDuration;
+        SecondsDuration = secondsDuration;
+    }
+
+    public string Artist
+    {
+        get => artist;
+        set
+        {
+            if (string.IsNullOrEmpty(value)||string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Artist name should be between 3 and 20 symbols.");
+            }
+            if (value.Length<minArtistNameLength||value.Length>maxArtistNameLength)
+            {
+                throw new ArgumentException("Artist name should be between 3 and 20 symbols.");
+            }
+
+            this.artist = value;
+        }
+    }
+
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Song name should be between 3 and 30 symbols.");
+            }
+            if (value.Length < minSongNameLength || value.Length > maxSongNameLength)
+            {
+                throw new ArgumentException("Song name should be between 3 and 30 symbols.");
+            }
+
+            this.name = value;
+        }
+    }
+
+    public int MinutesDuration
+    {
+        get => minutesDuration;
+        set
+        {
+            if (value<minMinutes||value>maxMinutes)
+            {
+                throw new ArgumentException("Song minutes should be between 0 and 14.");
+            }
+
+            this.minutesDuration = value;
+        }
+    }
+
+    public int SecondsDuration
+    {
+        get => secondsDuration;
+        set
+        {
+            if (value<minSeconds||value>maxSeconds)
+            {
+                throw new ArgumentException("Song seconds should be between 0 and 59.");
+            }
+
+            this.secondsDuration = value;
+        }
+    }
+
+    public long TotalDurationSeconds
+    {
+        get => totalDurationSeconds = this.MinutesDuration * 60 + this.SecondsDuration;
+    }
+}
+
---
+++ Exercises/10 INHERITANCE - EXERCISES/Inheritance-Exercises/04_Online_Radio_Database/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StartUp
+{
+    static void Main()
+    {
+        var listOfSongs = new List<Song>();
+        int numberOfSongs = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numberOfSongs; i++)
+        {
+            try
+            {
+                string[] input = Console.ReadLine().Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                if (input.Length<3)
+                {
+                    Console.WriteLine("Invalid song.");
+                    continue;
+                }
+                string artistName = input[0];
+                string songName = input[1];
+
+                string[] minutesAndSeconds = input[2].Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    int minutes = int.Parse(minutesAndSeconds[0]);
+                    int seconds = int.Parse(minutesAndSeconds[1]);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid song length.");
+                    continue;
+                }
+
+                Song song = new Song(artistName, songName, int.Parse(minutesAndSeconds[0]), int.Parse(minutesAndSeconds[1]));
+                Console.WriteLine("Song added.");
+                listOfSongs.Add(song);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        Console.WriteLine($"Songs added: {listOfSongs.Count}");
+        long timeInSecond = 0;
+        foreach (var song in listOfSongs)
+        {
+            timeInSecond += song.TotalDurationSeconds;
+        }
+
+        long hours = timeInSecond / 3600;
+        timeInSecond -= hours*3600;
+        long minutesEnd = timeInSecond / 60;
+        timeInSecond -= minutesEnd * 60;
+
+        Console.WriteLine($"Playlist length: {hours}h {minutesEnd}m {timeInSecond}s");
+       
+    }
+}
+
