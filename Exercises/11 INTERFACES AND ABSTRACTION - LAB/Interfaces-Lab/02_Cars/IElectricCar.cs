6456f6d6d3efbf0570df708719385decf9c67d0e
inser788 <inser7@abv.bg>
2018-03-01 10:43:16 +0200

    add 2 Exercises/11 INTERFACES AND ABSTRACTION - LAB/ 200/200

---
+++ Exercises/11 INTERFACES AND ABSTRACTION - LAB/Interfaces-Lab/02_Cars/IElectricCar.cs
@@ -0,0 +1,5 @@
+﻿public interface IElectricCar
+{
+    int Batterries { get; }
+}
+
---
+++ Exercises/12 INTERFACES AND ABSTRACTION - EXERCISES/Interfaces-Exercises/01_Define_an_Interface_IPerson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_Define_an_Interface_IPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
