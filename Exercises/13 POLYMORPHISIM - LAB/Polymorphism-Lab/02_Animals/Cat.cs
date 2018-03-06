0980c068afc92d66681b8ada08e22602752cd109
inser788 <inser7@abv.bg>
2018-03-06 16:32:15 +0200

    add 3 new Exercises/13 POLYMORPHISIM - LAB 300/300

---
+++ Exercises/13 POLYMORPHISIM - LAB/Polymorphism-Lab/02_Animals/Cat.cs
@@ -0,0 +1,12 @@
+﻿public class Cat : Animal
+{
+    public Cat(string name, string favouriteFood)
+        : base(name, favouriteFood)
+    {
+    }
+
+    public override string ExplainSelf()
+    {
+        return base.ExplainSelf() + "MEEOW";
+    }
+}
